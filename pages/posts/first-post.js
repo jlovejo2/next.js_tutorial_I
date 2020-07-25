d6a12f6103ed4951a1f95e847b78dba74f879ae6
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-25 18:37:18 -0500

    added layout to first post and then added styling to layout component

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,22 +1,25 @@
 import Link from "next/link";
 import Head from "next/head";
+import Layout from "../../components/layout";
 
 //Component can have any name
 //must be exported as a default function though
 export default function FirstPost() {
   return (
     <>
-      <Head>
-        <title>First Post</title>
-      </Head>
-      <h1>First Post</h1>
-      {
-        //link does client side navigation
-        //faster than default navigation by browser cause its using JavaScript
-      }
-      <Link href="/">
-        <a>Back to home</a>
-      </Link>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        {
+          //link does client side navigation
+          //faster than default navigation by browser cause its using JavaScript
+        }
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </Layout>
     </>
   );
 }
