c8dd7d3c25ab93a9fa822793e6f007d20f90ddca
James Lovejoy <james.lovejoy2@gmail.com>
2020-07-29 16:22:19 -0500

    fixed layout import

--- components/layout.js
+++ components/layout.js
@@ -1,4 +1,4 @@
-import Head from "next/Head";
+import Head from "next/head";
 import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/Link";
